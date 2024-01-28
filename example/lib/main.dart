e220a9ec757ed4fda8985691146bc657dcdb9e38
Abbas Hussein <112737126+Abbas1Hussein@users.noreply.github.com>
2024-01-28 17:10:02 +0300

    AbbasHussein

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,10 @@ import 'package:adp_desktop/adp_desktop.dart';
 
 import 'app/app.dart';
 import 'app/controllers/platforms_controller.dart';
+
 void main() async {
   final targetPlatform = await PlatformController().targetPlatform;
 
   DefaultsPlatformManager.initialize(targetPlatform: targetPlatform);
   runApp(const App());
 }
-
