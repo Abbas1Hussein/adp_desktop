b3a22315dc18805e10d7df90b668ca8a03f5498d
Abbas Hussein <112737126+Abbas1Hussein@users.noreply.github.com>
2024-01-28 15:12:03 +0300

    AbbasHussein

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:adp_desktop/adp_desktop.dart';
 
 import 'app/app.dart';
 import 'app/controllers/platforms_controller.dart';
-//////////////////////////////////////
 void main() async {
   final targetPlatform = await PlatformController().targetPlatform;
 
